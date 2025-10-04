2586c414067c4bbb0a9b8310f6d39b12992942fc
Gabriel Morales <90733936+GabrielMoraR@users.noreply.github.com>
2025-10-04 11:56:43 -0600

    Update database.js
    CAMBIO DE VARIABLE PASWWORD

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const sequelize = new Sequelize(
   process.env.DB_NAME || "tienda_db",
   process.env.DB_USER || "root",
-  process.env.DB_PASS || "",
+  process.env.DB_PASSWORD || "",
   {
     host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
