9c246279dbaccbc6eb020257d8fa35a4a3038f0d
Gabriel Morales <90733936+GabrielMoraR@users.noreply.github.com>
2025-10-10 12:56:07 -0600

    Update uploadCloudinary.js
    version 12:56

--- middleware/uploadCloudinary.js
+++ middleware/uploadCloudinary.js
@@ -1,3 +1,4 @@
+// /middleware/uploadCloudinary.js
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import { v2 as cloudinary } from "cloudinary";
@@ -5,14 +6,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// Configura Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Configura el almacenamiento con Cloudinary
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
@@ -21,8 +20,6 @@ const storage = new CloudinaryStorage({
   },
 });
 
-// Crea la instancia de multer con el storage configurado
 const upload = multer({ storage });
 
-// ✅ Exportación correcta para ES Modules
 export default upload;
